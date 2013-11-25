ef59deef25e39bd3bbf07e051410025e5c51c3cf
Jimzy Lui <jimzylui_desktop@stickyaznrabbit.com>
2013-11-24 19:15:50 -0500

    Initial Commit

---
+++ Thousand Words/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Thousand Words
+//
+//  Created by Jimzy Lui on 11/24/2013.
+//  Copyright (c) 2013 Jimzy Lui. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "TWAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([TWAppDelegate class]));
+    }
+}
